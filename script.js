cfda203a46622d76c3bb63dd89ea2540ac1ee06b
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-11-30 10:11:32 +0200

    Fixed variable name mistake in gameLoop()

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function gameLoop() {
         const gamepad = getGamepad();
         const stickValues = readAnalogSticks(gamepad);
         leftStickInfo.innerText = `Left Stick: X=${stickValues[0]}, Y=${stickValues[1]}`;
-        leftStickInfo.innerText = `Right Stick: X=${stickValues[2]}, Y=${stickValues[3]}`;
+        rightStickInfo.innerText = `Right Stick: X=${stickValues[2]}, Y=${stickValues[3]}`;
     } catch (error) {
         console.log(error);
     }
