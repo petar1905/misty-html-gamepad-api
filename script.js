eb4b781cd10c9ded6e94606ae32e5c9876a543af
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-12-01 17:17:14 +0200

    Added head rotation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let leftStickInfo = document.getElementById("leftStickInfo");
 let rightStickInfo = document.getElementById("rightStickInfo");
 
+let headRotation = [0, 0];
+
 function getGamepad() {
     const gamepads = navigator.getGamepads();
     for (let i = 0; i < gamepads.length; i++) {
@@ -19,12 +21,25 @@ function readAnalogSticks(gamepad) {
     return [leftStickX, leftStickY, rightStickX, rightStickY];
 }
 
+function mapAnalogValues(x, y) {
+    const minX = -1;
+    const maxX = 1;
+    const minY = -100;
+    const maxY = 100;
+    const scaledX = Math.floor((x - minX) / (maxX - minX) * (maxY - minY) + minY);
+    const scaledY = Math.floor((y - minX) / (maxX - minX) * (maxY - minY) + minY);
+    return [scaledX, scaledY];
+  }
+
 function gameLoop() {
     try {
         const gamepad = getGamepad();
         const stickValues = readAnalogSticks(gamepad);
+        const headValues = mapAnalogValues(stickValues[2], stickValues[3]);
+        headRotation[0] += headValues[0]
+        headRotation[1] += headValues[1]
         leftStickInfo.innerText = `Left Stick: X=${stickValues[0]}, Y=${stickValues[1]}`;
-        rightStickInfo.innerText = `Right Stick: X=${stickValues[2]}, Y=${stickValues[3]}`;
+        rightStickInfo.innerText = `Head Rotation: X=${headRotation[0]}, Y=${headRotation[1]}`;
     } catch (error) {
         console.error(error);
     }
