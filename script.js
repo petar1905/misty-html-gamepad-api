3b3a3650af037bcf686c1e638c7d19383653671d
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-12-07 14:49:37 +0200

    Updated bound-checking logic

--- script.js
+++ script.js
@@ -42,11 +42,10 @@ function gameLoop() {
         const gamepad = getGamepad();
         const stickValues = readAnalogSticks(gamepad, deadzone);
         const headValues = mapAnalogValues(stickValues[2], stickValues[3]);
-
+        let isWithinBounds;
         for (let i = 0; i <= 1; ++i) {
-            if (headValues[i] < 0 && headRotation[i] > -100) {
-                headRotation[i] += headValues[i]*sensitivity;
-            } else if (headValues[i] > 0 && headRotation[i] < 100) {
+            isWithinBounds = headRotation[i] > -100 && headRotation[i] < 100;
+            if (isWithinBounds) {
                 headRotation[i] += headValues[i]*sensitivity;
             }
         }
